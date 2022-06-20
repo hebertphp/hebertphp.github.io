395a803c7d146f817018326e384ea4ae54deefe4
Hebert <hebertphp@gmail.com>
2022-06-20 19:00:38 -0300

    Video

---
+++ video/js/script.js
@@ -0,0 +1,16 @@
+var i = 0;
+const video = ['mar.mp4', 'floresta.mp4', 'fundo.mp4', 'cachoeira.mp4', 'teclado.mp4', 'moto.mp4'];
+const vid = document.querySelector('#vid');
+const parag = document.querySelector('#parag');
+const ex = document.querySelector('#ex');
+var tempo = 15;
+// ex.addEventListener("click", tempo());
+
+function mudar() {
+    vid.src = "./video/" + video[i];
+    parag.innerHTML = "Video: <b>" + video[i] + "<b>";
+    i++;
+    if (i == video.length) { i = 0; }
+    setTimeout(function () { mudar() }, tempo * 1000);
+}
+mudar();
